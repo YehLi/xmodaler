f6ae30570bb28dfe54f8a4b24f62af808a3359d2
Winne2GO <9052195+winnechan@users.noreply.github.com>
2021-09-01 09:50:10 +0800

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,7 @@ extensions = [
     "sphinx.ext.mathjax",
     "sphinx.ext.viewcode",
     "sphinx.ext.githubpages",
-    #"torch"
+    "torch"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
